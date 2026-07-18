aa9396752c79f2830696709d9221b81756403f72
manohar barma <manoharbarma07@gmail.com>
2026-07-18 15:39:47 +0530

    feat: implement log file reading and severity level counting utility

--- log-analyzer/src/main.py
+++ log-analyzer/src/main.py
@@ -1,45 +1,46 @@
-import re
-
-
 def read_log(file_path: str) -> list:
     try:
         with open(file_path, "r") as file:
             data = file.readlines()
+        return data
     except FileNotFoundError:
         print("file not found")
     except Exception as e:
         print(f"failed to read data from {file_path} :: {e}")
-    return data
 
 
-# print(read_log("./log-analyzer/logs/application.log"))
+# print(type(read_log("./log-analyzer/logs/application.log")))
 
 
-def count_levels(data: list(str)) -> dict:
+def count_levels(data: list) -> dict:
     counts = {"INFO": 0, "WARNING": 0, "ERROR": 0,
               "UNKNOWN": 0, "TOTAL": len(data)}
     for log in data:
-        log_split = log.split()
-        if log_split[2] == "INFO":
-            counts["INFO"] += 1
-        elif log_split[2] == "WARNING":
-            counts["WARNING"] += 1
-        elif log_split[2] == "ERROR":
-            counts["ERROR"] += 1
-        else:
+        parts = log.split()
+        try:
+            if parts[2] == "INFO":
+                counts["INFO"] += 1
+            elif parts[2] == "WARNING":
+                counts["WARNING"] += 1
+            elif parts[2] == "ERROR":
+                counts["ERROR"] += 1
+            else:
+                counts["UNKNOWN"] += 1
+        except IndexError:
+            print("log level not found")
             counts["UNKNOWN"] += 1
     return counts
 
 
 # print(count_levels(read_log("./log-analyzer/logs/application.log")))
 
-def print_summary(count_levels: dict):
+def print_summary(counts: dict):
     print("========== Enterprise Log Analyzer ==========")
-    print(f"\nTotal Lines : {count_levels["TOTAL"]}")
-    print(f"\nINFO : {count_levels["INFO"]}")
-    print(f"WARNING : {count_levels["WARNING"]}")
-    print(f"ERROR : {count_levels["ERROR"]}")
-    print(f"UNKNOWN : {count_levels["UNKNOWN"]}")
+    print(f"\nTotal Lines : {counts["TOTAL"]}")
+    print(f"\nINFO : {counts["INFO"]}")
+    print(f"WARNING : {counts["WARNING"]}")
+    print(f"ERROR : {counts["ERROR"]}")
+    print(f"UNKNOWN : {counts["UNKNOWN"]}")
 
 
 print_summary(count_levels(read_log("./log-analyzer/logs/application.log")))
